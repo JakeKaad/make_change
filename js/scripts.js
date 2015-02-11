968d733dc4ff1a7349c55d9a5c6cbba6e9a9f3cc
Gabe Finch and Jake Kaad <kaadalac@gmail.com>
2015-02-11 15:58:29 -0800

    Add html && jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -46,6 +46,48 @@ var createChangeString = function(coinStrings) {
   }
 };
 
-var currencies = {
-  us: [ [ ], [ ] ],
-}
+$(function(){
+  $("#change_maker").submit(function(event){
+    var coins = [0, 0, 0, 0];
+    var userChange = parseFloat($("#change").val());
+    if ($("#25").is(':checked')) {
+      coins[0] = 25;
+    }
+    if ($("#10").is(':checked')) {
+      coins[1] = 10;
+    }
+    if ($("#5").is(':checked')) {
+      coins[2] = 5;
+    }
+    if ($("#1").is(':checked')) {
+      coins[3] = 1;
+    }
+
+    var coinAmounts = makeChange(userChange, coins);
+    for(var i=0; i < coinAmounts[0]; i++) {
+      $('#bank').append('<img src="img/quarter.png" class="coins"/>');
+    }
+    for(var i=0; i < coinAmounts[1]; i++) {
+      $('#bank').append('<img src="img/dime.png" class="coins"/>');
+    }
+    for(var i=0; i < coinAmounts[2]; i++) {
+      $('#bank').append('<img src="img/nickel.png" class="coins"/>');
+    }
+    for(var i=0; i < coinAmounts[3]; i++) {
+      $('#bank').append('<img src="img/penny.png" class="coins"/>');
+    }
+
+    var coinsText = coinString(coinAmounts);
+    $('#change_given').text(coinsText);
+    $('.results').toggle(function() {
+      $(this).fadeIn(400);
+    });
+
+    event.preventDefault();
+  });
+
+  $("#make_it_rain").click(function(){
+    $(".coins").animate( {margin: "0px 0px 0px 0px"}, "slow" );
+  });
+
+});
